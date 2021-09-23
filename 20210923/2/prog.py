999e213fb13dd89dcd29d72a0a59624e207a0f9f
Ilya Savitsky <ipsavitsky234@gmail.com>
2021-09-23 15:56:36 +0300

    changed 2

--- 20210923/2/prog.py
+++ 20210923/2/prog.py
@@ -1,10 +1,10 @@
 num = int(input())
 sum = 0
 while num > 0:
-    sum += num
-    if sum > 21:
-        break
-    num = int(input())
-    
-
-print(sum)
+	sum += num
+	if sum > 21:
+		print(sum)
+	break
+	num = int(input())
+else:
+	print(num)
