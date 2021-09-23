66f88227b254d3c348ed4a97c49f9326c62e3a7b
Ilya Savitsky <ipsavitsky234@gmail.com>
2021-09-23 16:02:16 +0300

    3 changed

--- 20210923/3/prog.py
+++ 20210923/3/prog.py
@@ -3,17 +3,19 @@ a = int(input())
 bot = a
 top = a + 3
 while a < top:
-    b = bot
-    while b < top:
-        sum_digit = 0
-        for n in str(a*b):
-            sum_digit += int(n)
-        if sum_digit == 6:
-            print(':=)', end=' ')
-            b += 1
-            continue
-        print(f'{a}*{b}={a*b}', end=' ')
-        b += 1
-    print()
-    a += 1
+	b = bot
+	while b < top:
+		sum_digit = 0
+		num = b
+		while num > 0:
+			sum_digit += num%10
+			num //= 10
+		if sum_digit == 6:
+			print('{a}*{b}=:=)', end=' ')
+			b += 1
+			continue
+		print(f'{a}*{b}={a*b}', end=' ')
+		b += 1
+	print()
+	a += 1
 
