87b5822388d32aa718b788e17e8b64a4291e14ff
Ilya Savitsky <ipsavitsky234@gmail.com>
2021-09-30 15:11:46 +0300

    30 sep task 1

---
+++ 20210930/1/prog.py
@@ -0,0 +1,10 @@
+l = [int(x) for x in input().split(',')]
+
+for i in range(len(l) - 1):
+    for j in range(len(l) - 1):
+        if (l[j]*l[j] % 100) > (l[j + 1]*l[j + 1] % 100):
+            l[j], l[j + 1] = l[j + 1], l[j]
+
+# l.sort(key=lambda x: (x**2) % 100)
+
+print(l)
