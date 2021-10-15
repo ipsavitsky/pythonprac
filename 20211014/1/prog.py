b0d234be781414d6fd0bf1b795c95d597b10dd2a
Ilya Savitsky <ipsavitsky234@gmail.com>
2021-10-15 14:30:16 +0300

    anther update

--- 20211014/1/prog.py
+++ 20211014/1/prog.py
@@ -1,11 +1,11 @@
 from fractions import Fraction
-val = eval(input())
-s = Fraction(val[0])
-w = Fraction(val[1])
-A = 0
-for i in range(val[2],-1,-1):
-	A += Fraction(s**i) * Fraction(val[2+val[2]-i+1])
-B = 0
-for i in range(val[2+val[2]+2],-1,-1):
-	B += Fraction(s**i) * Fraction(val[2+val[2]+2+val[2+val[2]+2]-i+1])
-print(A == w*B)
+inp = eval(input())
+S = Fraction(inp[0])
+W = Fraction(inp[1])
+A_vals = 0
+for i in range(inp[2], -1, -1):
+    A_vals += Fraction(S**i) * Fraction(inp[2+inp[2]-i+1])
+B_vals = 0
+for i in range(inp[2+inp[2]+2], -1, -1):
+    B_vals += Fraction(S**i) * Fraction(inp[2+inp[2]+2+inp[2+inp[2]+2]-i+1])
+print(A_vals == W*B_vals)
