fc1cba0701f43672e2947992c6a78fc81edb2dab
Ilya Savitsky <ipsavitsky234@gmail.com>
2022-02-28 19:33:50 +0300

    you can now choose algorithm

--- 20220228/1/prog.py
+++ 20220228/1/prog.py
@@ -1,12 +1,18 @@
 import textdistance as td
 
-def dist(s1: str, s2: str) -> int:
-    return td.levenshtein(s1, s2)
+def dist(s1: str, s2: str, type_str: str) -> int:
+    match type_str:
+        case 'L':
+            return td.levenshtein(s1, s2)
+        case 'D':
+            return td.damerau_levenshtein(s1, s2)
+        case _:
+            return -1
 
 s1 = input()
 s2 = input()
+len_type = input()
 
 
-res = dist(s1, s2)
-
+res = dist(s1, s2, len_type)
 
