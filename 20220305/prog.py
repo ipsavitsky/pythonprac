1859db2471c3d49e2a8076d6a17fdf8a8b7b5a07
Ilya Savitsky <ipsavitsky234@gmail.com>
2022-03-05 23:23:17 +0300

    20220305 first task

---
+++ 20220305/prog.py
@@ -0,0 +1,25 @@
+from dataclasses import dataclass
+import readline
+import shlex
+import cmd
+
+@dataclass
+class Monster():
+    pass
+
+
+
+class DungeonGame(cmd.Cmd):
+    prompt = '>>'
+    def do_add():
+        pass
+
+    def do_show():
+        pass
+
+    def do_move():
+        pass
+
+    def do_attack():
+        pass
+
